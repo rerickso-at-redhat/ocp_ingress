54607118825c78b9f3e21eec4e39e004d0284dc8
Ryan Erickson <rerickso@redhat.com>
2025-10-02 23:15:43 -0700

    Add metallb operator installation via github defaults

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,6 @@ oc apply -f projects.yml
 cd hello-app && ./deploy.sh
 cd ..
 
+git clone --depth 1 --single-branch --branch release-4.18 https://github.com/openshift/metallb-operator.git
+oc apply -f metallb-operator/bin/metallb-operator.yaml
+
