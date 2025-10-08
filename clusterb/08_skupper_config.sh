a1d46a6ffc1b3074ca9e85dc2a189ea145aaa40f
Ryan Erickson <rerickso@redhat.com>
2025-10-08 16:44:16 -0700

    skupper configs moved to deploy.sh until CRDs are created

--- clusterb/08_skupper_config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# establish a site
-skupper token redeem clusterb-token.yaml
-# create a listener for app1 (this allows clustera to send traffic for app2 here)
-skupper listener create app2 8080
-# create a connector for app1 (to send traffic to clustera for app1)
-skupper connector create app1 8080 --workload deployment/app1
-
