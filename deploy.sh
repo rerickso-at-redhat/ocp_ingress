d563e185c1a441ad7ea60247a7f1cbba30b5b13b
Ryan Erickson <rerickso@redhat.com>
2025-10-08 15:26:46 -0700

    Add skupper commands

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,7 @@ CLUSTERB="https://api.cluster-xm972.dynamic.redhatworkshops.io:6443"
 
 set -e
 
-
-## CLUSTER A / APP1 ##
+# CLUSTER A / APP 1
 
 oc login --web --server=$CLUSTERA || (echo "Unable to log into clustera: $CLUSTERA" && exit 1)
 
@@ -30,10 +29,12 @@ sleep 60
 oc apply -f 05_shared-gateway.yaml
 oc apply -f 06_app1-gateway-http-route.yaml
 oc apply -f 07_app2-gateway-http-route.yaml
+#echo "NOTE: The Red Hat Service Interconnect Operator *MUST* be installed to proceed..."
+#./08_skupper_config.sh
 cd ..
 
 
-## CLUSTER B / APP2 ##
+# Cluster B / APP 2
 
 oc login --web --server=$CLUSTERB || (echo "Unable to log into clusterb: $CLUSTERB" && exit 1)
 
@@ -57,5 +58,41 @@ sleep 60
 oc apply -f 05_shared-gateway.yaml
 oc apply -f 06_app1-gateway-http-route.yaml
 oc apply -f 07_app2-gateway-http-route.yaml
+#echo "NOTE: The Red Hat Service Interconnect Operator *MUST* be installed to proceed..."
+#./08_skupper_config.sh
 cd ..
+
+
+# Red Hat Service Interconnect (Skupper)
+
+## Cluster A
+oc login --server=$CLUSTERA || oc login --web --server=$CLUSTERA
+
+### App 1
+skupper --namespace app1 site create --enable-link-access app1
+skupper --namespace app1 token issue app1-token.yml
+skupper --namespace app1 connector create app1 8080 --workload deployment/app1
+
+### App 2
+skupper --namespace app2 site create --enable-link-access app2
+
+
+## Cluster B
+oc login --server=$CLUSTERB || oc login --web --server=$CLUSTERB
+
+### App 1
+skupper --namespace app1 site create --enable-link-access app1
+skupper --namespace app1 token redeem app2-token.yml
+
+### App 2
+skupper --namespace app2 site create --enable-link-access app2
+skupper --namespace app2 token issue app2-token.yml
+skupper --namespace app2 connector create app2 8080 --workload deployment/app2
+
+
+## Cluster A
+oc login --server=$CLUSTERA || oc login --web --server=$CLUSTERA
+
+### App 2
+skupper --namespace app2 token redeem app2-token.yml
 
