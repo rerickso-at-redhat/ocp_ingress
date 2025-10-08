949617f37eb38493bda27066547b6f02193e426b
Brando Marlow <brandon.n.marlow@gmail.com>
2025-10-08 13:42:36 -0700

    fix skupper instructions

--- clustera/08_skupper_config.sh
+++ clustera/08_skupper_config.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-# establish a site
+# establish a site from clustera ns/app1 to clusterb ns/app1
+# on clustera in ns/app1
+oc project app1
 skupper site create [sitename] --enable-link-access
 # create a token so cluster b can join
 skupper token issue clusterb-token.yaml
-# create a listener for app1 (this allows clusterb to send traffic for app1 here)
-skupper listener create app1 8080
-# create a connector for app2 (to send traffic to cluster b for app2)
-skupper connector create app2 8080 --workload deployment/app2
+
+###
+# now go to clusterb and redeem the token
+# oc project app1
+# skupper token redeem clusterb-token.yaml
+
+# create a connector for app1 (allowing trafic to come from clusterb)
+skupper connector create app1 8080 --workload deployment/app1
+
+
+
+# create a listener for app2 (this allows clustera to send traffic for app1 here)
+oc project app2
+skupper listener create app2 8080
 
--- clusterb/08_skupper_config.sh
+++ clusterb/08_skupper_config.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-# establish a site
-skupper token redeem clusterb-token.yaml
-# create a listener for app1 (this allows clustera to send traffic for app2 here)
-skupper listener create app2 8080
-# create a connector for app1 (to send traffic to clustera for app1)
-skupper connector create app1 8080 --workload deployment/app1
+# establish a site from clustern ns/app2 to clustera ns/app2
+# on clusterb in ns/app2
+oc project app2
+skupper site create [sitename] --enable-link-access
+# create a token so cluster a can join
+skupper token issue clustera-token.yaml
+
+# now go redeem the token on clustera
+# oc project app2
+# skupper token redeem clustera-token.yaml
+
+# create a connector for app2 (allowing trafic to come from clustera)
+skupper connector create app2 8080 --workload deployment/app2
+
+# create a listener for app1 (this allows clusterb to send traffic for app1 to clustera)
+oc project app1
+skupper listener create app1 8080
 
