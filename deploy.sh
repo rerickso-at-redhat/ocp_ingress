d8e542316daa66f8004d6308e058e259c96681f5
Ryan Erickson <rerickso@redhat.com>
2025-10-22 22:10:02 -0700

    Improve wait_for functions and add approve_installplan function.

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-CLUSTERA="https://api.cluster-275lg.dynamic.redhatworkshops.io:6443"
-CLUSTERB="api.cluster-xm972.dynamic.redhatworkshops.io:6443"
+CLUSTERA="api.cluster-52rbh.dynamic.redhatworkshops.io:6443"
+CLUSTERB="api.cluster-fgt68.dynamic.redhatworkshops.io:6443"
 CLUSTERA_CONTEXT="default/$(echo $CLUSTERA | sed s/\\./-/g)/admin" # NOTE: Highly assumptive - demo purposes only
 CLUSTERB_CONTEXT="default/$(echo $CLUSTERB | sed s/\\./-/g)/admin" # NOTE: Highly assumptive - demo purposes only
 
 function main () {
 	set -ex
-	
+
 	# Authentication # NOTE: Utilizing a very generic authentication pattern - feel free to manually swap this out with your needs or preferences
 	echo "Logging into Cluster A"
 	oc login --web --server=$CLUSTERA
@@ -17,48 +17,48 @@ function main () {
 	oc login --web --server=$CLUSTERB
 	oc project default
 
-	# Cluster A	
+	# Cluster A
 	echo "Using Cluster A"
 	oc config use-context $CLUSTERA_CONTEXT
-	
+
 	## Namespaces
 	oc apply -f namespaces.yml
-	
+
 	## App 1
 	deploy_app app1
-	
+
 	## Infrastructure Manifests
 	cd clustera
 	apply_manifests
 	cd ..
-	
+
 	# Cluster B
 	echo "Using Cluster B"
 	oc config use-context $CLUSTERB_CONTEXT
-	
+
 	## Namespaces
 	oc apply -f namespaces.yml
-	
+
 	## App 2
 	deploy_app app2
-	
+
 	## Infrastucture Manifests
 	cd clusterb
 	apply_manifests
 	cd ..
-	
-	
+
+
 	# Red Hat Service Interconnect (Skupper API & CLI Mixture)
-	
+
 	## Cluster A
 	oc config use-context $CLUSTERA_CONTEXT
 	skupper --namespace app1 token issue app1-token.yml
-	
+
 	## Cluster B
 	oc config use-context $CLUSTERB_CONTEXT
 	skupper --namespace app2 token issue app2-token.yml
 	skupper --namesapce app1 token redeem app1-token.yml
-	
+
 	## Cluster A
 	oc config use-context $CLUSTERA_CONTEXT
 	skupper --namespace app2 token redeem app2-token.yml
@@ -74,14 +74,34 @@ function deploy_app () {
 }
 
 
+function wait_for_operator () {
+	OPERATOR_NAME=$1
+        until [ $(oc get csv -n openshift-operators | grep $OPERATOR_NAME > /dev/null; echo $?) == 0 ]; do echo "Waiting for operator: $OPERATOR_NAME"; sleep 5; done
+	oc wait --for jsonpath='{.status.phase}'=Succeeded --timeout=10m -n openshift-operators $(oc get csv -n openshift-operators -oname | grep $OPERATOR_NAME)
+
+}
+
+
+function wait_for_installplan () {
+        until [ $(oc -n openshift-operators get installplan | grep false > /dev/null; echo $?) == 1 ]; do echo "Waiting for installplan ..."; sleep 5; done
+}
+
+
+# NOTE: Demo purposes only as this blindly approves any pending installplans in the openshift-operators namespace
+function approve_installplans () {
+	for ip in $(oc -n openshift-operators get ip -o=jsonpath='{.items[?(@.spec.approved==false)].metadata.name}'); do
+		oc -n openshift-operators patch installplan $ip --type merge -p '{"spec":{"approved":true}}';
+	done
+}
+
+
 function apply_manifests () {
         # MetalLB (https://docs.redhat.com/en/documentation/openshift_container_platform/4.19/html/networking_operators/metallb-operator)
         echo "NOTE: IP failover *MUST* be disabled before installing the MetalLB Operator..."
         echo "      (Operator Install)    https://docs.redhat.com/en/documentation/openshift_container_platform/4.19/html/networking_operators/metallb-operator#metallb-operator-install"
         echo "      (IP failover removal) https://docs.redhat.com/en/documentation/openshift_container_platform/4.19/html-single/configuring_network_settings/index#nw-ipfailover-remove_configuring-ipfailover"
         oc apply -f 10-metallb-operator.yml
-        until [ $(oc get csv -n openshift-operators | grep metallb-operator > /dev/null; echo $?) == 0 ]; do echo "Waiting for the MetalLB operator"; sleep 5; done
-        oc wait --for jsonpath='{.status.phase}'=Succeeded --timeout=10m -n openshift-operators $(oc get csv -n openshift-operators -oname | grep metallb)
+	wait_for_operator metallb-operator
         echo "NOTE: The MetalLB Operator *MUST* be installed to proceed..."
         ## The MetalLB namespace *MUST* have the cluster-monitoring label for BGP/BFD metrics to appear in Prometheus...
         oc label ns metallb-system "openshift.io/cluster-monitoring=true"
@@ -89,13 +109,14 @@ function apply_manifests () {
         oc apply -f 14-metallb-ipaddresspool.yml
         oc apply -f 16-metallb-bgppeer.yml
         oc apply -f 18-metallb-bgpadvertisement.yml
-        
+
 	# Gateway API (https://gateway-api.sigs.k8s.io/)
         echo "NOTE: The cluster *MUST* have \"GatewayClass\" available (GA in OCP 4.19+) to proceed..."
         oc apply -f 30-gateway-gatewayclass.yml
         ## 4.19 will cause an operator installation that can be waited for
-        until [ $(oc get csv -n openshift-operators | grep servicemeshoperator > /dev/null; echo $?) == 0 ]; do echo "Waiting for the Service Mesh operator"; sleep 5; done
-        oc wait --for jsonpath='{.status.phase}'=Succeeded --timeout=10m -n openshift-operators $(oc get csv -n openshift-operators -oname | grep servicemeshoperator)
+        wait_for_operator servicemeshoperator
+        approve_installplans
+        wait_for_operator servicemeshoperator
         ## <= 4.18, same operator install? # TODO
         #
         #
@@ -104,12 +125,10 @@ function apply_manifests () {
         oc apply -f 36-gateway-httproute-app2.yml
 	
 	# Red Hat Service Interconnect (https://docs.redhat.com/en/documentation/red_hat_service_interconnect/2.1)
-	# TODO: the latest rhsi seems to need the latest service mesh. however, it doesnt install automatically and instead creates an install plan (b/c the auto ossm install on 4.19 sets it to manual?)
-        # Probably need to downgrade skupper or manually install service mesh before skupper (vs automatic with GatewayClass) or change ossm to automatic upgrade
-        # Review the manual install plan for operators servicemeshoperator3.v3.1.2, skupper-operator.v2.1.1-rh-3. Once approved, the following resources will be created in order to satisfy the requirements for the components specified in the plan. Click the resource name to view the resource in detail.
         oc apply -f 50-skupper-operator.yml
-        until [ $(oc get csv -n openshift-operators | grep skupper-operator > /dev/null; echo $?) == 0 ]; do echo "Waiting for the Service Interconnect (skupper) operator"; sleep 5; done
-        oc wait --for jsonpath='{.status.phase}'=Succeeded --timeout=10m -n openshift-operators $(oc get csv -n openshift-operators -oname | grep skupper-operator)
+	wait_for_installplan
+	approve_installplans
+	wait_for_operator skupper-operator
         echo "NOTE: The Red Hat Service Interconnect Operator *MUST* be installed to proceed..."
         oc apply -f 52-skupper-app1.yml
         oc apply -f 54-skupper-app2.yml
