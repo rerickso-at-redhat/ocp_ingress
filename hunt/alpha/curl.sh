d80a8b18dc43e106deefb68e653bb467cb2f4cfb
Ryan Erickson <rerickso@redhat.com>
2025-12-15 20:55:35 -0800

    Remove original static manifests and other unused files

--- hunt/alpha/curl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-while true; do
-	curl http://apps.alpha.sandbox728.opentlc.com/app1
-	sleep 1
-done
--- hunt/beta/curl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-while true; do
-	curl http://projecta.anycast.goguybrush.com/app1
-	sleep 1
-done
